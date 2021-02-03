723ca54a1c2fc44780a3e9c90a907a84f5982682
hanifazzuhdi <hnfhanif52@gmail.com>
2021-02-03 16:17:31 +0700

    initial project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    /**
+     * Function for create new user
+     *
+     */
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User Berhasil dibuat',
+            'data' => $user
+        ], 201)->header('Accept', 'application/json');
+    }
+
+    /**
+     * Function for login
+     *
+     */
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->input('email'))->firstOrFail();
+
+        if (Hash::check($request->input('password'), $user->password)) {
+            $user->update([
+                'api_token' => base64_encode(Str::random(40))
+            ]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Login Berhasil',
+                'data' => [
+                    'user' => $user,
+                    'token' => $user->api_token
+                ]
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Login Gagal Password Anda Salah',
+            ], 400);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Str;
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/key', function () {
+    return Str::random(32);
+});
+
+
+// route definition
+$router->post('/register', 'AuthController@register');
+$router->post('/login', 'AuthController@login');
